24cc9fd604757d5e6fa4d5adf6aaa06061cbcdb0
Mosey <moseev.es@ya.ru>
2021-01-29 11:41:25 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <h1 class="title">Поля ввода компонентов RGB и бегунки:</h1>
+    <color @upd_color="update" color="red" />
+    <color @upd_color="update" color="green" />
+    <color @upd_color="update" color="blue" />
+  </div>
+</template>
+
+<script>
+import color from "./components/color.vue";
+
+export default {
+  name: "App",
+  components: {
+    color,
+  },
+  data() {
+    return {
+      red: 255,
+      green: 255,
+      blue: 255,
+    };
+  },
+  methods: {
+    update: function ({ color, value }) {
+      this[color] = +value;
+      document.body.style.backgroundColor = `rgb(${this.red}, ${this.green}, ${this.blue})`;
+      document.querySelector(".title").style.color = `rgb(${255 - this.red}, ${
+        255 - this.green
+      }, ${255 - this.blue})`;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  width: 400px;
+  margin: 100px auto 0 auto;
+  text-align: center;
+}
+input {
+  zoom: 150%;
+}
+</style>
---
+++ src/components/color.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="color">
+    <div class="color__text">{{ color.toUpperCase() }}:</div>
+    <input
+      class="color__input"
+      type="number"
+      min="0"
+      max="255"
+      v-model="value"
+    />
+    <input type="range" min="0" max="255" v-model="value" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "color",
+  props: {
+    color: String
+  },
+  data() {
+    return {
+      value: 255,
+    };
+  },
+  computed: {},
+  watch: {
+    value: function () {
+      this.$emit("upd_color", {
+        color: this.color,
+        value: this.value,
+      });
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+.color__text {
+  font-size: 24px;
+  text-align: left;
+}
+.color__input {
+  width: 50px;
+}
+.color {
+  display: grid;
+  grid-auto-flow: column;
+  grid-auto-columns: 1fr;
+}
+.color:not(:last-child) {
+  margin-bottom: 15px;
+}
+</style>
